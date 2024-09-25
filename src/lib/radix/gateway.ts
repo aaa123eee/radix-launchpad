eae6453f29f08f5d20e7351ed35b8c90b8b748f4
v.livshits <v.livshits@sdventures.com>
2024-09-25 11:20:00 +0200

    added structure

--- src/lib/radix/gateway.ts
+++
@@ -1,4 +0,0 @@
-import { GatewayApiClient } from '@radixdlt/babylon-gateway-api-sdk';
-import { rdt } from './rdt'; // Assuming rdt is defined in this file
-
-export const gatewayApi = GatewayApiClient.initialize(rdt.gatewayApi.clientConfig);
