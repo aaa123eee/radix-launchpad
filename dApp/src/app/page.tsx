56c23e0b19146a57122c31551c5e4a984871f6ca
v.livshits <v.livshits@sdventures.com>
2024-09-26 12:25:45 +0200

    added nice loader

--- dApp/src/app/page.tsx
+++ dApp/src/app/page.tsx
@@ -8,7 +8,11 @@ export default function TokensList() {
   const { data: tokens, isLoading } = api.token.getAll.useQuery();
 
   if (isLoading) {
-    return <div>Loading...</div>;
+    return (
+      <div className="flex h-screen items-center justify-center">
+        <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-yellow-500"></div>
+      </div>
+    );
   }
 
   return <CoinsGrid tokens={tokens} />;
