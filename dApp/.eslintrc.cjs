04e40ed829f58f53791154e7617cbfcdc8e866dd
aaa_eee <aaa_eee@icloud.com>
2024-09-26 16:46:40 +0200

    soften linting rules

--- dApp/.eslintrc.cjs
+++ dApp/.eslintrc.cjs
@@ -12,6 +12,9 @@ const config = {
   ],
   rules: {
     "@typescript-eslint/array-type": "off",
+    "@typescript-eslint/prefer-nullish-coalescing": "off",
+    "@typescript-eslint/no-empty-object-type": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
     "@typescript-eslint/consistent-type-definitions": "off",
     "@typescript-eslint/no-unsafe-assignment": "off",
     "@typescript-eslint/no-floating-promises": "off",
